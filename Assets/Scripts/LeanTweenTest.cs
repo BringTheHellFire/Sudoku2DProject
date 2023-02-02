eef21561a65fc3c01e67f9ef32854058060be20b
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2023-02-02 19:03:12 +0100

    Playing with Teeen

--- Assets/Scripts/LeanTweenTest.cs
+++ Assets/Scripts/LeanTweenTest.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class LeanTweenTest : MonoBehaviour
 {
-    [SerializeField] private float ButtonMoveHeight = 0.5f;
-    [SerializeField] private float ButtonMovementTime = 0.5f;
+    [SerializeField] private GameObject levelSelectionPanel;
+    [SerializeField] private float buttonMovementTime = 0.5f;
 
     public void ButtonReact_OnClick()
     {
-        LeanTween.moveY(gameObject, transform.position.y + ButtonMoveHeight, ButtonMovementTime).setEaseShake();
+        transform.LeanScale(new Vector3(1.2f, 1.2f, 0f), buttonMovementTime).setEaseInQuart().setLoopPingPong(1).setOnComplete(TriggerLevelSelection);
+
     }
+
+    private void TriggerLevelSelection()
+    {
+        levelSelectionPanel.SetActive(true);
+        levelSelectionPanel.LeanScale(new Vector3(1f, 1f, 1f), buttonMovementTime).setEaseInExpo();
+    }
+
 }
