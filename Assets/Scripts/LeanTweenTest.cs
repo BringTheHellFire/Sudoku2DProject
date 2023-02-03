9fe48d61c9324570d66d0e52b743bccf904615d4
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2023-02-03 12:22:02 +0100

    Add animations to MainMenuScene

--- Assets/Scripts/LeanTweenTest.cs
+++ Assets/Scripts/LeanTweenTest.cs
@@ -7,10 +7,38 @@ public class LeanTweenTest : MonoBehaviour
     [SerializeField] private GameObject levelSelectionPanel;
     [SerializeField] private float buttonMovementTime = 0.5f;
 
+    [SerializeField] private GameObject decorations;
+
+    [SerializeField] private GameObject backButton;
+    [SerializeField] private GameObject playButton;
+
     public void ButtonReact_OnClick()
     {
-        transform.LeanScale(new Vector3(1.2f, 1.2f, 0f), buttonMovementTime).setEaseInQuart().setLoopPingPong(1).setOnComplete(TriggerLevelSelection);
+        playButton.LeanScale(new Vector3(0f, 0f, 0f), buttonMovementTime).setEaseOutQuart().setOnStart(MoveDownDecoration);
+        //transform.LeanScale(new Vector3(1.2f, 1.2f, 0f), buttonMovementTime).setEaseInQuart().setLoopPingPong(1).setOnComplete(TriggerLevelSelection);
+    }
 
+    public void BackButton_OnClick()
+    {
+        playButton.LeanScale(new Vector3(1f, 1f, 1f), buttonMovementTime).setEaseOutQuart().setOnStart(MoveUpDecoration);
+    }
+
+    private void MoveDownDecoration()
+    {
+        decorations.transform.LeanMoveLocal(new Vector2(0f, -701f), buttonMovementTime).setEaseOutQuart().setOnComplete(EnableBackButton);
+    }
+    private void MoveUpDecoration()
+    {
+        decorations.transform.LeanMoveLocal(new Vector2(0f, 800f), buttonMovementTime).setEaseOutQuart().setOnStart(DisableBackButton);
+    }
+
+    private void EnableBackButton()
+    {
+        backButton.LeanScale(new Vector3(1f, 1f, 1f), buttonMovementTime).setEaseOutQuart();
+    }
+    private void DisableBackButton()
+    {
+        backButton.LeanScale(new Vector3(0f, 0f, 0f), buttonMovementTime).setEaseOutQuart();
     }
 
     private void TriggerLevelSelection()
