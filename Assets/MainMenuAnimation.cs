0968fcfbf73f78f0003bcbd338f75f2c9b80f668
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2023-02-12 17:19:53 +0100

    Add some effect prefabs

--- Assets/MainMenuAnimation.cs
+++ Assets/MainMenuAnimation.cs
@@ -8,10 +8,29 @@ public class MainMenuAnimation : MonoBehaviour
     [SerializeField] private GameObject title;
     [SerializeField] private GameObject playButton;
 
+    [SerializeField] private ParticleSystem theParticleSystem;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void SetStarPosition()
+    {
+        Debug.Log("Start!");
+        var particles = new ParticleSystem.Particle[theParticleSystem.main.maxParticles];
+        var currentAmount = theParticleSystem.GetParticles(particles);
+
+        // Change only the particles that are alive
+        for (int i = 0; i < currentAmount; i++)
+        {
+            particles[i].position = new Vector3(i, i, 0);
+            Debug.Log("Particle: " + i + " " + particles[i].position);
+        }
+
+        // Apply the particle changes to the Particle System
+        theParticleSystem.SetParticles(particles, currentAmount);
     }
 
     // Update is called once per frame
