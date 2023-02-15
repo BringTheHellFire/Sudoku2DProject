a593984f2e1ea31a35ac84262c15b6b48b274707
BringTheHellFire <73886873+BringTheHellFire@users.noreply.github.com>
2023-02-16 00:12:25 +0100

    Fix reward text

--- Assets/Scripts/RewardManager.cs
+++ Assets/Scripts/RewardManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class RewardManager : MonoBehaviour
@@ -10,6 +11,8 @@ public class RewardManager : MonoBehaviour
 
     [SerializeField] private int rewardAmount = 0;
 
+    [SerializeField] private TextMeshProUGUI rewardMessage;
+
     private void Awake()
     {
         fieldSpawner.gridIsFilled.AddListener(GiveVictoryReward);
@@ -18,6 +21,39 @@ public class RewardManager : MonoBehaviour
     private void GiveVictoryReward()
     {
         playerInfo.coinAmount += rewardAmount * LevelSettings.difficultySetting;
+        int powerUp = UnityEngine.Random.Range(1, 3);
+        switch (powerUp)
+        {
+            case 1:
+                playerInfo.fieldPowerUpAmount += 1;
+                break;
+            case 2:
+                playerInfo.rowPowerUpAmount += 1;
+                break;
+            case 3:
+                playerInfo.rowPowerUpAmount += 1;
+                break;
+        }
+        SetDisplayMessage(rewardAmount * LevelSettings.difficultySetting, powerUp);
+    }
+
+    private void SetDisplayMessage(int amountOfCoins, int powerUpIndex)
+    {
+        rewardMessage.text = "You won: " + amountOfCoins + ".";
+        string powerUpName = "";
+        switch (powerUpIndex)
+        {
+            case 1:
+                powerUpName = "Fill one field";
+                break;
+            case 2:
+                powerUpName = "Fill one row";
+                break;
+            case 3:
+                powerUpName = "Fill one column";
+                break;
+        }
+        rewardMessage.text += "\nAnd a power up: " + powerUpName + ".";
     }
 
 }
